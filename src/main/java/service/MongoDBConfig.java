c3cbf4fa69d49fdc2ced002a84e10482f8eddf3c
ViniciusS <vinicius.scandura@jala.university>
2024-04-18 21:11:11 -0300

    Atualização de tratativa de erro para conexão com o banco

--- src/main/java/service/MongoDBConfig.java
+++ src/main/java/service/MongoDBConfig.java
@@ -4,12 +4,23 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
 
+import javax.swing.*;
+
 public class MongoDBConfig {
     private static final String CONNECTION_STRING = "mongodb+srv://viniciusscandura:ItgShkVQVHdgLvi4@cluster0.anpxsj6.mongodb.net/";
     private static final String DATABASE_NAME = "FInalProject";
 
+
     public static MongoDatabase getDatabase() {
-        MongoClient mongoClient = MongoClients.create(CONNECTION_STRING);
-        return mongoClient.getDatabase(DATABASE_NAME);
+        JFrame frame = new JFrame();
+        frame.setAlwaysOnTop(true);
+        try {
+            MongoClient mongoClient = MongoClients.create(CONNECTION_STRING);
+            return mongoClient.getDatabase(DATABASE_NAME);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(frame, "Erro ao conectar ao banco de dados.");
+            System.out.println(e.getMessage());
+            return null;
+        }
     }
 }
